9a1677d99c0c64512dd6d1d1823ef14e10d8434f
dhruv <dhruv20345@gmail.com>
2024-03-12 23:35:44 +0530

    HUGGINGFACEHUB_API_TOKEN

--- src/controllers/chat/chatbot.js
+++ src/controllers/chat/chatbot.js
@@ -53,8 +53,7 @@ export const askQuestion = async (req, res) => {
         const method = 'post';
         const url = `${CHATBOT_API_END_POINT}/ask`;
         const headers = {
-            'Authorization': "Bearer " + config.HUGGINGFACEHUB_API_TOKEN,
-            // 'Authorization': req.headers['authorization'],
+            'Authorization': req.headers['authorization'],
             'Content-Type': 'application/json'
         };
         console.log(url)

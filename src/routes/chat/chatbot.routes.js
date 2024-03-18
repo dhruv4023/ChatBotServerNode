bdc29e62e46c87aefd4d06c9fbfef4ffed2491ac
dhruv <dhruv20345@gmail.com>
2024-03-18 14:45:17 +0530

    route

--- src/routes/chat/chatbot.routes.js
+++ src/routes/chat/chatbot.routes.js
@@ -7,9 +7,7 @@ import { askQuestion, createTmpChain } from '../../controllers/chat/chatbot.js';
 
 const router = express.Router();
 
-router.post("/ask/question", askQuestion)
-// router.post("/ask/question", verifyTokenAndRole(["user", "admin"]), askQuestion)
+router.post("/ask/question", verifyTokenAndRole(["user", "admin"]), askQuestion)
 router.post("/create/tmp/chain", verifyTokenAndRole(["user", "admin"]), upload.array("files"), createTmpChain)
-// router.post("/query/ask/", askQuestion)
 
 export default router;
